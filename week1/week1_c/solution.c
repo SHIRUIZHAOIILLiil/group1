1f3441ba04ce1cdc9a00340b2284e5e9948b84ef
SHIRUIZHAOIILLiil <146215624+SHIRUIZHAOIILLiil@users.noreply.github.com>
2023-10-09 19:55:44 +0000

    c

--- week1/week1_c/solution.c
+++ week1/week1_c/solution.c
@@ -1,4 +1,7 @@
 #include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
 
 void rectanglar(){
     float length1 = 0, length2 = 0, area = 0;
@@ -13,18 +16,29 @@ void rectanglar(){
 
 void reverse(){
     char name[15],rev[15];
+    int length = sizeof(name), j = 0;
+    
     printf("please enter your name: \n");
     scanf("%s",name);
-    int length = sizeof(name);
-    printf("%d\n",length);
-    printf("%c",name[0]);
-    // for(int i=0;i<sizeof(name);i++)
-    // {
-    //     rev[i] = name[length-1];
-    //     printf("%s",rev);
-    //     length--;
-    // }
-    //printf("the name is %s, and the reverse name is %s: \n",name, rev);
+   
+    while(name[j]!='\0'){
+        j++;
+    }
+    j = j-1 ;
+    int def = j;
+
+
+    for(int i = 0;i<=def;i++){
+        rev[i] = name[j];
+        j--;
+        if(i == def){
+            rev[i+1] = '\0';
+        }
+    }
+   
+    printf("the name is %s, and the reverse name is %s: \n", name, rev);
+
+
 }
 
 int main(){
